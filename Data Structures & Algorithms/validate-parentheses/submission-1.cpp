cac0f4c58abd6d80cbbea84ab6c6d03cfca2d178
kushagra58 <2023pietcrkushagra037@poornima.org>
2026-08-09 16:13:28 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='{' || s[i]== '[' || s[i]=='('){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                char c=st.top();
+                if((s[i]=='}' && c=='{')||(s[i]==']' && c=='[') || (s[i]==')' && c=='(')){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+        
+    }
+};
